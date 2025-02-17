21e8553bff55ee9a418f35d9eff02f26f92b77b2
htaek2 <shtlrlszld@gmail.com>
2025-02-17 10:29:24 +0900

    asdasd

--- crawling.py
+++ crawling.py
@@ -1,5 +1,9 @@
 # Test
+
 def elice_javascript():
+
   this file is elice's javascript func
+
   if __name__ == '__main__':
+
     elice_javascript()
--- crawling2.py
+++ crawling2.py
@@ -1,4 +1,7 @@
 def perfect_time_check() :
+
   He is murdering time!
+
   if __name__ == '__main__' :
+    
     perfect_time_check()
